2e749bc14820997d03c94555cb5364b6fcb88334
Francisco García Delgado <francisco.garciadel@iesdoctorbalmis.com>
2021-04-03 16:47:56 +0200

    Cambios en ej4

--- FranciscoGarcia-actividades2-UT5/Ejercicio 4/act4UT5-2FranciscoGarcia.js
+++ FranciscoGarcia-actividades2-UT5/Ejercicio 4/act4UT5-2FranciscoGarcia.js
@@ -1,13 +1,19 @@
 'use strict';
 
-let trabajador = new Array();
+function IntroducirDatos(){
+    let nombre = prompt("Introduce el nombre: ");
+    let horas = prompt("Introduce el total de horas trabajadas: ");
+    let precioHora = prompt("Introduce el precio por hora trabajada: ")
+    let trabajador = new Array(nombre, Number(horas), Number(precioHora));
+    return trabajador;
+}
 
-function Ejercicio4(...trabajador) {
+function Ejercicio4(...IntroducirDatos) {
 
-    let nombre, horas, precioHora, salarioBruto;
-
-    for (let i in trabajador) {
-        [nombre, horas, precioHora] = trabajador[i];
+    let nombre, horas, precioHora, salarioBruto;  
+    
+    for (let i in IntroducirDatos) {
+        [nombre, horas, precioHora] = IntroducirDatos[i];
         salarioBruto = horas * precioHora;
         console.log("Nombre: " + nombre);
         console.log("Salario Bruto Semanal: " + salarioBruto);
@@ -17,4 +23,4 @@ function Ejercicio4(...trabajador) {
     }
 }
 
-Ejercicio4(["Antonio Gómez", 40, 8],["Jose Antúnez", 40, 10]);
+Ejercicio4(IntroducirDatos());
