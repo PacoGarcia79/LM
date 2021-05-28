d904919a552c145b57a01ba8da6e2a6038c40732
Francisco García Delgado <francisco.garciadel@iesdoctorbalmis.com>
2021-05-28 17:50:30 +0200

    cambios en ejercicio 3

--- FranciscoGarcia-actividades3-UT5/Ejercicio 3/act3UT5-3FranciscoGarcia.js
+++ FranciscoGarcia-actividades3-UT5/Ejercicio 3/act3UT5-3FranciscoGarcia.js
@@ -1,67 +1,50 @@
 `use strict`
-/*
-let campos = " ";
-campos += "<form id='formulario' onsubmit='return validar()'>"
-campos += "<div class='tabla'><div class='fila'>"
-campos += "<label for='email'>Email: </label>"
-campos += "<input type='email' id='email' name='email' pattern = '[A-Za-z0-9\.]+@iesdoctorbalmis.com' required></div>"
-campos += "<div class='fila'>"
-campos += "<label for='contraseña'>Contraseña: </label>"
-campos += "<input type='password' id='contraseña' name='contraseña' pattern = '[0-9]{8}' required>"
-campos += "</div><div class='fila'>"
-campos += "<input type='submit' id='boton' value='Enviar'>"
-campos += "</div></div></form>"
 
-document.getElementById("inicio").innerHTML = campos;
-*/
+window.onload = CreaFormulario();
 
-let inicio = document.getElementById("inicio");
+function AñadeElemento(padre, elemento, clase, textoContenido) {
+    let capa = document.createElement(elemento);
+    capa.classList.add(clase);
+    capa.textContent = textoContenido;
+    padre.appendChild(capa);
 
-let formulario = document.createElement("form");
-formulario.id = "formulario";
+    return capa;
+}
 
-inicio.appendChild(formulario);
+function CreaFormulario() {
+    let inicio = document.getElementById("inicio");
+    let formulario = document.createElement("form");
+    formulario.id = "formulario";
+    inicio.appendChild(formulario);
 
-let tabla = document.createElement("div");
-tabla.classList.add("tabla");
-formulario.appendChild(tabla);
+    let tabla = AñadeElemento(formulario, "div", "tabla", "");
+    let filaUno = AñadeElemento(tabla, "div", "fila", "");
+    AñadeElemento(filaUno, "label", "labelUno", "Email: ");
 
-let filaUno = document.createElement("div");
-filaUno.classList.add("fila");
-tabla.appendChild(filaUno);
+    let email = AñadeElemento(filaUno, "input", "email", "");
+    email.type = "email";
+    email.id = "email";
+    email.name = "email";
+    email.pattern = "[A-Za-z0-9\.]+@iesdoctorbalmis.com";
+    email.required = true;
 
-let labelUno = document.createElement("label");
-labelUno.textContent = "Email: ";
-filaUno.appendChild(labelUno);
-let email = document.createElement("input");
-email.type = "email";
-email.id = "email";
-email.name = "email";
-email.pattern = "[A-Za-z0-9\.]+@iesdoctorbalmis.com"; 
-email.required = true;
-filaUno.appendChild(email);
+    let filaDos = AñadeElemento(tabla, "div", "fila", "");
+    AñadeElemento(filaDos, "label", "labelDos", "Contraseña: ");
 
-let filaDos = document.createElement("div");
-filaDos.classList.add("fila");
-tabla.appendChild(filaDos);
+    let passw = AñadeElemento(filaDos, "input", "passw", "");
+    passw.type = "password";
+    passw.id = "contraseña";
+    passw.name = "contraseña";
+    passw.pattern = "[0-9]{8}";
+    passw.required = true;
 
-let labelDos = document.createElement("label");
-labelDos.textContent = "Contraseña: ";
-filaDos.appendChild(labelDos);
-let passw = document.createElement("input");
-passw.type = "password";
-passw.id = "contraseña";
-passw.name = "contraseña";
-passw.pattern = "[0-9]{8}"; 
-passw.required = true;
-filaDos.appendChild(passw);
+    let filaTres = AñadeElemento(tabla, "div", "fila", "");
 
-let filaTres = document.createElement("div");
-filaTres.classList.add("fila");
-tabla.appendChild(filaTres);
+    let boton = AñadeElemento(filaTres, "input", "boton", "");
+    boton.type = "submit";
+    boton.id = "boton";
+    boton.value = "Enviar";
 
-let boton = document.createElement("input");
-boton.type = "submit";
-boton.id = "boton";
-boton.value = "Enviar";
-filaTres.appendChild(boton);
+}
+
+
